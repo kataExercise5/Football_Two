91c2e5c3b560144feb6b27f44621072687f6ee2e
Bill Filliater <bill.filliater@hotmail.com>
2025-11-05 14:40:26 -0500

    Adding DataBase project and DB objects

---
+++ Models/Game.cs
@@ -0,0 +1,20 @@
+﻿namespace Football_Two.Models
+{
+    public class Game
+    {
+        public int gameId { get; set; }
+        public int visitingTeamId { get; set; }
+        public int homeTeamId { get; set; }
+        public bool stadium {  get; set; }
+        public DateOnly datePlayed { get; set; }
+        public int dayOfWeek { get; set; }
+        public int visitingTeamScore { get; set; }
+        public int homeTeamScore { get; set; }
+        public int scheduleWeek { get; set; }
+
+        public Game()
+        {
+
+        }
+    }
+}
